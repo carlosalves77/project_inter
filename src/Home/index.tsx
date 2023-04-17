867b59c5569c4282c3c51fec97064292e809675b
Carlos Alves <carlosaalves07@outlook.com>
2023-04-17 20:56:03 -0300

    feat: first commit

---
+++ src/Home/index.tsx
@@ -0,0 +1,11 @@
+import {Text} from 'react-native';
+
+import * as S from './styles';
+
+export function Home() {
+  return (
+    <S.Container>
+      <Text>Welcome to React Native!</Text>
+    </S.Container>
+  );
+}
