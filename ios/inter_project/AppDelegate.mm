612e251868cf6857b4adabd530c481edc398aab7
Carlos Alves <carlosaalves07@outlook.com>
2023-05-04 23:44:24 -0300

    fix: change project name

--- ios/inter_project/AppDelegate.mm
+++ ios/inter_project/AppDelegate.mm
@@ -6,7 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"inter_project";
+  self.moduleName = @"Projeto Inter";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
--- ios/inter_projectTests/inter_projectTests.m
+++ ios/inter_projectTests/inter_projectTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface inter_projectTests : XCTestCase
+@interface ProjetoInterTests : XCTestCase
 
 @end
 
-@implementation inter_projectTests
+@implementation ProjetoInterTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
